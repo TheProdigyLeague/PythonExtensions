8f5ac4b607d7f9d44cfdbbff74fb5c676c35ebc8
Z4 <30985576+TheProdigyLeague@users.noreply.github.com>
2023-02-16 16:54:01 -0800

    Python Labs
    literal dlls, pkgs, mods, and envs...

---
+++ labs.py
@@ -0,0 +1,14 @@
+# Anacanda.nav
+```json
+[
+    {
+        "level": "INFO",
+        "line": 266,
+        "message": "appusermodelid: 0",
+        "method": "__init__",
+        "module": "__init__",
+        "path": "$ Python_ext\\lib\\site-packages\\anaconda_navigator\\widgets\\main_window\\__init__.py",
+        "time": "2023-02-14 18:35:00"
+    }
+]
+```
